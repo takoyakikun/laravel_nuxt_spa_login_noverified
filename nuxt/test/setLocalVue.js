de4f99fe996eddf8363c0c59793c69e0d3679eb2
Kouki Uchida <uchidakouki264@gmail.com>
2021-06-23 23:54:03 +0900

    nuxtテストの共通処理を修正

--- nuxt/test/setLocalVue.js
+++ nuxt/test/setLocalVue.js
@@ -1,11 +1,12 @@
 import { createLocalVue } from '@vue/test-utils'
-import Vuetify from 'vuetify'
 import Vuex from 'vuex'
 import VueRouter from 'vue-router'
+import Vuetify from 'vuetify'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
 localVue.use(VueRouter)
+localVue.use(Vuetify)
 
 const router = new VueRouter()
 const vuetify = new Vuetify()
