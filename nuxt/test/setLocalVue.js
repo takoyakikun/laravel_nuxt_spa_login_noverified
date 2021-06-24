1a6025a648739c217a0fa332c8141f9d84237e46
Kouki Uchida <uchidakouki264@gmail.com>
2021-06-24 23:54:55 +0900

    nuxtテストのheadのテスト設定を追加

--- nuxt/test/setLocalVue.js
+++ nuxt/test/setLocalVue.js
@@ -2,11 +2,13 @@ import { createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import VueRouter from 'vue-router'
 import Vuetify from 'vuetify'
+import VueMeta from 'vue-meta'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
 localVue.use(VueRouter)
 localVue.use(Vuetify)
+localVue.use(VueMeta, { keyName: 'head' })
 
 const router = new VueRouter()
 const vuetify = new Vuetify()
